1fc5737f8b3caa950bbeba6262ae5b6bcc6ff3c8
monster <differentcodemonster@gmail.com>
2021-12-10 01:01:49 +1030

    initial first

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test npm dayuntilxmas
+
+## install
+
+```
+$ npm install dayuntilxmas
+```
+
+### Caveat
+
+### keyword
---
+++ index.js
@@ -0,0 +1,12 @@
+const dayUntilXmas = () => {
+    const timeInADay = 24 * 60 * 60 * 1000;
+    const now = new Date();
+    const thisYear = now.getFullYear();
+    let dateOfChristmas = new Date(thisYear, 11, 24).getTime();
+    if (dateOfChristmas < now - timeInADay) {
+        dateOfChristmas = new Date(thisYear + 1, 11, 24).getTime();
+    }
+    return Math.abs(Math.ceil((dateOfChristmas - now) / timeInADay));
+}
+
+modeule.export = dayUntilXmas;
