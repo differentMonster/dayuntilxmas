2498cca4fc93082afc7a340fa45c78c31f60be6c
monster <differentcodemonster@gmail.com>
2021-12-13 02:06:34 +1030

    Plugins Text: test first folder

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // export * from '.core/index'
 // export * from './models/index'
-export * from './shaders/index'
+// export * from './shaders/index'
+
+export function greetPerson(name) {
+    return `Hello ${name}`;
+}
